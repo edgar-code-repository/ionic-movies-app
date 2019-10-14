45955ef69667d1909cc7549ed247aefb2e5c7bba
ECD <ecofre@gmail.com>
2019-10-13 23:23:45 -0300

    Se agrega listado de peliculas en pantalla inicial

---
+++ src/app/model/movie.ts
@@ -0,0 +1,8 @@
+export class Movie {
+    id: number;
+    title: string;
+    poster: string;
+    year: number;
+    plot: string;
+    idGenre: number;
+}
--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,11 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Movie } from './../../model/movie';
+
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.page.html',
   styleUrls: ['./movies.page.scss'],
 })
 export class MoviesPage implements OnInit {
+
+  moviesList: Movie[] = [
+    {
+      id: 1,
+      title: 'Star Wars',
+      poster: 'https://images-na.ssl-images-amazon.com/images/I/81w4kfIpSjL._SY550_.jpg',
+      year: 1977,
+      plot: '',
+      idGenre: 0
+    },
+    {
+      id: 2,
+      title: 'Saturday Night Fever',
+      poster: 'https://images-na.ssl-images-amazon.com/images/I/51HH4WHN8VL.jpg',
+      year: 1977,
+      plot: '',
+      idGenre: 0
+    },
+    {
+      id: 3,
+      title: 'Blade Runner',
+      poster: 'https://cdn.shopify.com/s/files/1/0627/1477/products/Blade_Runner080618PF_8f704f19-4808-47e5-93dc-224062b4d96f_large.jpg?v=1543504514',
+      year: 1982,
+      plot: '',
+      idGenre: 0
+    },
+    {
+      id: 4,
+      title: 'The Terminator',
+      poster: 'https://cdn.shopify.com/s/files/1/0627/1477/products/Blade_Runner080618PF_8f704f19-4808-47e5-93dc-224062b4d96f_large.jpg?v=1543504514',
+      year: 1984,
+      plot: '',
+      idGenre: 0
+    },
+    {
+      id: 5,
+      title: 'The Matrix',
+      poster: 'https://bloximages.chicago2.vip.townnews.com/stardem.com/content/tncms/assets/v3/editorial/9/1b/91b8775d-f35a-5f99-a3d1-9cb36d97279e/5c48d82b6bfb7.image.jpg?resize=500%2C773',
+      year: 1999,
+      plot: '',
+      idGenre: 0
+    }
+  ];
+  genresList = [];
+  selectedGenre = 0;
 
   constructor() { }
 
